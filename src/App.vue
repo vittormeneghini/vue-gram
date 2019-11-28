55d8050ebf0af1ea4d5ebb4195d013a3d18990a0
Vittor Hugo Meneghini <vittor.meneghini@gmail.com>
2019-11-28 08:24:13 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header>
+      <h3>VueGram</h3>
+    </Header>
+
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue'
+import Card from './components/card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Card
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
